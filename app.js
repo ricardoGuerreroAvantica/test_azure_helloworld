ab8e10abead3301efd0e110906065830a151589d
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-12-10 14:39:17 -0600

    no message

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 var express = require('express');
-var session = require('express-session');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-//var routes = require('./routes/index');
 var app = express();
-
-app.use(express.static('public/images'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session({
-  secret: 'NODE-SECRET',
-  name: 'nodecookie',
-  resave: false,
-  saveUninitialized: false
-}));
 
 // Routes
 app.get('/', function(req, res) {

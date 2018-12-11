27b25623bcdc66f301e30da5a08072bfcba26462
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-12-11 15:41:06 -0600

    no message

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var app = express();
+// Based on http://expressjs.com/en/starter/hello-world.html
+// https://creativecommons.org/licenses/by-sa/3.0/us/
+//
+// Extended to support configurable port and message.
 
-// Routes
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+const express = require('express')
 
-// Listen
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('Listening on localhost:'+ port);
+const app = express()
+const config = require('./config.json')
+
+app.get('/', function (req, res) {
+  res.send(config.message||'Hello World!')
+})
+
+app.listen(config.port, function () {
+  console.log('listening on port ' + String(config.port||3000))
+})
